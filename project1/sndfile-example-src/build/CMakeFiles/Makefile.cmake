43ccc6b2fed755d3dd9273e838041de34c5f5401
Diogo Torrinhas <59964065+diogotorrinhas@users.noreply.github.com>
2022-10-31 00:04:57 +0000

    update

--- project1/sndfile-example-src/build/CMakeFiles/Makefile.cmake
+++ project1/sndfile-example-src/build/CMakeFiles/Makefile.cmake
@@ -45,6 +45,7 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
+  "CMakeFiles/lossyencode.dir/DependInfo.cmake"
   "CMakeFiles/wav_effects.dir/DependInfo.cmake"
   "CMakeFiles/wav_quant.dir/DependInfo.cmake"
   "CMakeFiles/ex2.dir/DependInfo.cmake"
---
+++ project1/sndfile-example-src/build/CMakeFiles/lossyencode.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/diogo/Desktop/IC/project1/sndfile-example-bin/lossyencode"
+  "/home/diogo/Desktop/IC/project1/sndfile-example-bin/lossyencode.pdb"
+  "CMakeFiles/lossyencode.dir/lossyencode.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lossyencode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
